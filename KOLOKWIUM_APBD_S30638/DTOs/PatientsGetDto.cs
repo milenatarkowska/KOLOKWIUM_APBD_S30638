9266e30d073ac021493416eb56b6202124986991
s30638 <s30638@pjwstk.edu.pl>
2026-06-07 11:31:49 +0200

    Added dto and db context

---
+++ KOLOKWIUM_APBD_S30638/DTOs/PatientsGetDto.cs
@@ -0,0 +1,47 @@
+﻿using KOLOKWIUM_APBD_S30638.Entities;
+
+namespace KOLOKWIUM_APBD_S30638.DTOs;
+
+public class PatientsGetDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Phone { get; set; }
+    public List<AppointmentsGetDto> Appointments { get; set; }
+}
+
+public class AppointmentsGetDto
+{
+    public int AppointmentId { get; set; }
+    public List<DoctorsGetDto> Doctors { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Status { get; set; }
+    public List<Appointment_ServicesGetDto> AppointmentServices { get; set; }
+    
+}
+
+public class DoctorsGetDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialization { get; set; }
+    public String Phone { get; set; }
+}
+
+public class Appointment_ServicesGetDto
+{
+    public int Quantity {get; set;}
+    public DateTime PerformedAt {get; set;}
+    public List<Medical_ServicesGetDto> MedicalServices { get; set; }
+    
+}
+
+public class Medical_ServicesGetDto
+{
+    public int ServiceId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int DurationMinutes { get; set; }
+}
---
+++ KOLOKWIUM_APBD_S30638/Data/AppointmentsContext.cs
@@ -0,0 +1,18 @@
+﻿using KOLOKWIUM_APBD_S30638.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KOLOKWIUM_APBD_S30638.Data;
+
+public class AppointmentsContext : DbContext
+{
+    public AppointmentsContext(DbContextOptions<AppointmentsContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Appointments> Appointments { get; set; }
+    public DbSet<Appointment_Services> Appointment_Services { get; set; }
+    public DbSet<Doctors> Doctors { get; set; }
+    public DbSet<Medical_Services> Medical_Services { get; set; }
+    public DbSet<Patients> Patients { get; set; }
+}
